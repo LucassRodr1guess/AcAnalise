7fb56acab7ca6d05d4bf5eadf08538661d10eef0
LucassRodr1guess <84428281+LucassRodr1guess@users.noreply.github.com>
2022-06-23 13:05:04 -0300

    Add files via upload

---
+++ analise.js
@@ -0,0 +1,56 @@
+class Lanches {
+    constructor(hamburguer, queijo, alface, molho, cebola, picles, pao){
+        this.hamburguer = true;      
+        this.queijo = True;
+        this.alface = True;
+        this.molho = True;
+        this.cebola = True;
+        this.picles = True;
+        this.pao = True;
+    }
+}
+
+class Combo{
+    constructor(bebida, batata)
+
+}
+
+
+
+
+class McLancheAlegre extends Lanches{
+    constructor(,hamburguer, hamCarne, queijo, alface, batata, fruta, molho, cebola, picles, pao, bebida){
+        this.hamCarne = 1;
+        this.queijo = true;
+        this.batataP = true;
+        this.fruta = true;
+        this.pao = true;
+    }
+}
+class combo{
+    constructor(batata, bebida){
+        this.batataM = true;
+        this.batataP = false;
+        this.bebida = true;
+    }
+    preco(if (batataP = false;) {
+        this.preco = preco
+    })
+
+class BigLanche{
+    constructor(hamCarne, hamFrango, queijo, alface, batata, fruta, molho, cebola, picles, pao, bebida){
+        this.hamCarne = 2;
+        this.queijo = true;
+        this.alface = true;
+        this.molho = true;
+        this.cebola = true;
+        this.picles = true;
+        this.pao = true;
+        this.batataM = true;
+        this.bebida = true;
+    }
+    preco()
+}
+
+}
+
